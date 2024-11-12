c18ae9742396c31cd055cf9ba27aef4a856f397a
Wanda Valadares WandaWanda1@users.noreply.github.com <wandawanda@gmail.com>
2024-11-12 16:12:37 +0100

    rails task master

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,23 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+end
